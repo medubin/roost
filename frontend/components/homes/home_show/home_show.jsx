afe169d93a4890ee10520f20f2d9900e37a58ea9
Matt Dubin <medubin@gmail.com>
2017-04-04 18:33:03 -0700

    added users to show page

--- frontend/components/homes/home_show/home_show.jsx
+++ frontend/components/homes/home_show/home_show.jsx
@@ -4,19 +4,35 @@ import { Link, withRouter } from 'react-router';
 class HomeShow extends React.Component {
   constructor(props) {
     super(props)
-
   }
 
   componentWillMount() {
     this.props.fetchActiveHome(this.props.params.homeId);
   }
 
+  renderUsers() {
+    if (this.props.activeHome.users == undefined) {
+      return
+    }
+    console.log(this.props.activeHome)
+    let users = this.props.activeHome.users.map((user, key) => {
+
+      return <li key={key}>{user.username}</li>
+    })
+    return <ul>{users}</ul>
+  }
+
   render() {
     console.log(this.props.activeHome)
     return <div>
       Name: {this.props.activeHome.name}
+      <br/>
       Address: {this.props.activeHome.address}
+      {this.renderUsers()}
+
       </div>
+
+
   }
 
 }
